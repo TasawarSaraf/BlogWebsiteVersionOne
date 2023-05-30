06f8cb001a774f6b1324c283524ca45578a42856
TasawarSaraf <47578444+TasawarSaraf@users.noreply.github.com>
2023-05-29 23:32:17 -0400

    Made the home page, about page, & contact page
    Also set up the get routes for each page and also set up EJS for displaying the contact for the home, about, and contact pages. Lastly, created partials for header and footer to avoid repetition code.

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent = "Welcome to my blog! I'm Tasawar Saraf, a passionate computer science student at Lehigh University. As an aspiring web developer, I'm constantly seeking opportunities to enhance my skills and delve deeper into the fascinating world of technology. This blog serves as a testament to my dedication and growth, showcasing my proficiency in Node.js, Express.js, and EJS. Here, you'll find a collection of informative and engaging articles, tutorials, and insights, where I'll share my knowledge and experiences in web development. Join me on this exciting journey as we explore the limitless possibilities of the digital realm together. Let's dive in and discover the art of crafting exceptional web experiences!";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -16,7 +16,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", (req,res)=>{
+  res.render('home', {homeContent: homeStartingContent});
+})
 
+
+app.get("/about", (req,res)=>{
+  res.render('about', {aboutContent: aboutContent});
+})
+
+
+app.get("/contact", (req,res)=>{
+  res.render('contact', {contactContent: contactContent});
+})
 
 
 
